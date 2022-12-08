13d00d0562b29f931637cba14899e5fc122ca4ed
jayashripjadhav <jayashri9794@gmail.com>
2022-12-08 15:38:24 +0530

    hello variable 01_variable_assign file added
    hello variable 01_variable_assign file added

---
+++ 01_variable_assign.js
@@ -0,0 +1,17 @@
+var myfullName;
+console.log(myfullName);
+myfullName=("Jayashri Jadhav");
+console.log(myfullName);
+myfullName=("Jayashri Pandurang Jadhav");
+console.log(myfullName);
+var myLove=("I love only JS");
+console.log(myLove);
+var firstName=("Jayashri");
+var lastName=("Jadhav");
+var age=("29");
+var address=("At post Waki Tal Sangola Dist Solapur");
+var pinCode=("413307");
+var gender=("Female");
+var is_married=("Yes");
+console.log(firstName , lastName ,age ,address,pinCode,gender,is_married);
+
---
+++ variable.js
@@ -0,0 +1,3 @@
+var myName;
+myName="Jayashri";
+console.log(myName);
